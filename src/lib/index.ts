1878a374e85bc186b52435387d3110e1277704d1
taha sadough <tahasadoughdev@gmail.com>
2026-05-30 12:25:22 +0330

    My bad, I forgot to remove the profile link export from index.ts.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -9,7 +9,6 @@ export { default as metalicShape } from './assets/images/metalic-shape-backgroun
 export { default as Badge } from './components/ui/badge.svelte';
 export { default as Button } from './components/ui/button.svelte';
 export { default as Card } from './components/ui/card.svelte';
-export { default as ProfileLink } from './components/ui/profile-link.svelte';
 export { default as Navbar } from './components/layout/nav/navbar.svelte';
 export { default as Footer } from './components/layout/footer.svelte';
 export { default as MousetrailProvider } from './components/providers/mousetrail-provider.svelte';
