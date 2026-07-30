475783426d2206aed9b26e6f599939ddf98049d1
Taha Sadough <tsadough.eng@gmail.com>
2026-07-30 15:59:03 +0330

    removed svelte

--- README.md
+++
@@ -1,3 +0,0 @@
-## About This Project
-
-This repository holds the source code for my personal website
--- src/lib/components/layout/nav/nav.ts
+++
@@ -1,5 +0,0 @@
-export const navLinks = [
-	{ href: '#about', name: 'About' },
-	{ href: '#why-me', name: 'Why Me' },
-	{ href: '#contact', name: 'Contact' }
-] as const satisfies { href: string; name: string }[];
