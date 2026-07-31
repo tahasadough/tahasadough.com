b3cfb0428d7b980717b3947adf184bbf8e15aa4c
Taha Sadough <tsadough.eng@gmail.com>
2026-07-31 15:43:47 +0330

    perf: bundle app.js into a single module

--- static/js/navbar.js
+++
@@ -1,9 +0,0 @@
-let ticking = false;
-window.addEventListener("scroll", () => {
-  if (ticking) return;
-  ticking = true;
-  requestAnimationFrame(() => {
-    document.getElementById("navbar")?.classList.toggle("scrolled", window.scrollY > 50);
-    ticking = false;
-  });
-});
