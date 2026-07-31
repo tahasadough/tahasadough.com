7d7d440281b54a8c877ad070f4f1893cf888adfb
Taha Sadough <tsadough.eng@gmail.com>
2026-07-31 20:10:07 +0330

    feat: add security headers and server timeouts

--- cmd/tahasadough/main.go
+++ cmd/tahasadough/main.go
@@ -33,8 +33,11 @@ func run() error {
 
 	srv := &http.Server{
 		Addr:              ":" + port,
-		Handler:           mux,
+		Handler:           web.SecurityHeaders()(mux),
 		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       120 * time.Second,
 	}
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
