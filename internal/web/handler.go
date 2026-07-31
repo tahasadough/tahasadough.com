5d4017a7d33f144a7dd051f8eff316a420d4fe3a
Taha Sadough <tsadough.eng@gmail.com>
2026-07-31 20:36:39 +0330

    refactor: simplify caching, drop ETag middleware

--- internal/web/handler.go
+++ internal/web/handler.go
@@ -15,12 +15,16 @@ const sitemapXML = `<?xml version="1.0" encoding="UTF-8"?>
 	</url>
 </urlset>`
 
+const htmlCache = "public, max-age=3600"
+
 func Home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Cache-Control", htmlCache)
 	_ = home.Page().Render(r.Context(), w)
 }
 
 func Sitemap(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/xml; charset=utf-8")
+	w.Header().Set("Cache-Control", htmlCache)
 	_, _ = w.Write([]byte(sitemapXML))
 }
--- internal/web/routes.go
+++ internal/web/routes.go
@@ -6,29 +6,19 @@ import (
 	"github.com/tahasadough/tahasadough.com/static"
 )
 
-const (
-	cacheHTML       = "public, max-age=300, must-revalidate"
-	cacheRevalidate = "public, max-age=3600, must-revalidate"
-	cacheImmutable  = "public, max-age=31536000, immutable"
-)
-
 func Routes(mux *http.ServeMux) {
-	html := CacheControl(cacheHTML)
-	etag := ETag()
-	mux.Handle("GET /{$}", html(etag(http.HandlerFunc(Home))))
-	mux.Handle("GET /sitemap.xml", html(etag(http.HandlerFunc(Sitemap))))
+	mux.Handle("GET /{$}", http.HandlerFunc(Home))
+	mux.Handle("GET /sitemap.xml", http.HandlerFunc(Sitemap))
 }
 
 func StaticRoutes(mux *http.ServeMux) {
 	fs := http.FileServer(http.FS(static.FS))
-	revalidate := CacheControl(cacheRevalidate)
-	etag := ETag()
+	mux.Handle("/", cache(fs, "public, max-age=86400"))
+}
 
-	staticMux := http.NewServeMux()
-	staticMux.Handle("GET /images/", CacheControl(cacheImmutable)(fs))
-	staticMux.Handle("GET /js/", revalidate(etag(fs)))
-	staticMux.Handle("GET /favicon.ico", CacheControl(cacheImmutable)(fs))
-	staticMux.Handle("GET /robots.txt", revalidate(etag(fs)))
-
-	mux.Handle("/", CacheControl(cacheRevalidate)(staticMux))
+func cache(h http.Handler, value string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", value)
+		h.ServeHTTP(w, r)
+	})
 }
